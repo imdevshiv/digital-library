7c9d9243b082884de55328277ff42c8e25a0b97f
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-05-01 11:48:54 +0530

    enabling firebase storing file data in mogoDB

--- index.js
+++ index.js
@@ -2,125 +2,196 @@ const dotenv = require('dotenv').config(); // to get .env values
 const express = require('express');
 //const path = require('path');
 const ejs = require("ejs"); // require embeded javascript
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // to help get form data from frontend
+const multer = require('multer'); // to catch files from form data from frontend
 const mongoose = require("mongoose"); // to build connection between server and mongoDB
 const session = require("express-session"); // to store user session details
 const mongoStore = require("connect-mongo"); // it helps in storing session datas
 const bcrypt = require("bcrypt"); // it will help to encrypt user password
-
-
+var randomstring = require("randomstring"); // to create random string 
+const { initializeApp } = require('firebase/app'); // require firebase
+const { getStorage, ref, uploadBytes, getDownloadURL } = require("firebase/storage"); // getting required services from firestore
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
+const fireBaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(); //points to root directory of 
+const imagesRef = ref(storage, 'images/'); // create images reference or images (child)folder inside root directory
+const documentsRef = ref(storage, 'documents/') //create documents reference or documents (child)folder inside root directory
 // build connection with mongo
 const mongodb_url = process.env.MONGO_URL; // getting mongo url
 const mongodbOptions = { useNewUrlParser: true }; // mongo options need during making connection
 const connectToMongoDB = async function () {
     try {
-     mongoose.connect(mongodb_url,mongodbOptions);
-      console.log("connected Success fully");
+        await mongoose.connect(mongodb_url, mongodbOptions);
+        console.log("connected Success fully");
     } catch (error) {
-      console.log(error); 
+        console.log(error);
     }
 }
-connectToMongoDB();
+connectToMongoDB(); // connect to mongoDB
 
-const app = express();  // make express app firts
+const app = express();  // make express app first
 app.use(bodyParser.urlencoded({ extended: true })); // enables to get data from client side
 app.use(express.static("public")); // to serve as a basic file to front side browser
 app.set("view engine", "ejs"); // to render ejs files
+
+//create multer instances -- that will be used as a middleware 
+const upload = multer({
+    // set file limit
+    limits: {
+        fileSize: 1024 * 1024 * 10 // 10 MB max size
+    },
+    fileFilter: function (req, file, callBackFn) {
+        const fileType = file.mimetype;
+        if (fileType === "application/pdf" || "image/jpeg" || "image/png") {
+            callBackFn(null, true);
+        } else {
+            callBackFn(new Error("You have not entered valid file"));
+        }
+    },
+    storage: multer.memoryStorage()// store files in the memory as buffer
+})
+
 // using session middle are for every routes - to get user Id and to authenticate them
 app.use(session({
     secret: process.env.SECRET_SESSION,
-    resave:false,
-    saveUninitialized:false,
+    resave: false,
+    saveUninitialized: false,
     store: mongoStore.create({
         mongoUrl: mongodb_url,
         collectionName: "Session"
     }),
-    cookie:{
-        maxAge: 1000*60*60*24*7 // 1 Week expairy date // 1000=1sec, 1000*60 = 1min , 1000*60*60 = 1hr ,1000*60*60*24*7 = 1 week  
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 Week expairy date // 1000=1sec, 1000*60 = 1min , 1000*60*60 = 1hr ,1000*60*60*24*7 = 1 week  
         // cookies will be stored for one week and then automatically deleted.
-    } 
+    }
 }));
 
 
-  // first define a schema
+// first define a schema
 const usersSchema = new mongoose.Schema({
-    name : String,
+    name: String,
     email: String,
-    userType : String,
+    userType: String,
     password: String,
 });
-
 const User = new mongoose.model("User", usersSchema);
 
-
-
-
+const documentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    },
+    uploadedBy: {
+        type: String,
+        ref: "users"
+    },
+    relatedSem: Number,
+    docOriginalName: String,
+    docGivenName: String,
+    uploadDate: {
+        type: Date,
+        default: Date.now
+    },
+    uploadedDocUrl: {
+        required: true,
+        type: String
+    },
+    docType: {
+        required: true,
+        type: String
+    },
+    usesType: {
+        type: String,
+        default: "study_material"
+    }
+})
+// creates a new collection in the database named with documents
+const Document = new mongoose.model("Document", documentSchema);
 // GET --- HOME ROUTE 
-app.get("/",async (req,res)=>{
+app.get("/", async (req, res) => {
     if (req.session.isAuth === true) {
         // user_id is stored in session as req.session.user_ID 
         //check user type -- teacher or student 
-       const foundUser = await User.findOne({_id:req.session.user_ID});
-       if (foundUser) {
-            if (foundUser.userType === "Student"){
+        const foundUser = await User.findOne({ _id: req.session.user_ID });
+        if (foundUser) {
+            if (foundUser.userType === "Student") {
                 res.redirect(`/student/${req.session.user_ID}`);
-            }else{
+            } else {
                 res.redirect(`/teacher/${req.session.user_ID}`);
             }
-       }else{
-         res.render("home");
-       }
+        } else {
+            res.render("home");
+        }
     }
-    else{
+    else {
         res.render("home");
     }
 })
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     // res.sendFile('public/registration.html' , {root : __dirname});
     res.render("registration");
 });
-app.post("/register",async (req,res)=>{
-   try {
+app.post("/register", async (req, res) => {
+    try {
         const name = req.body.name;
         const email = req.body.email;
         const userType = req.body.userType;
         const password = req.body.password;
-        const foundUser = await User.findOne({email : email});
+        const foundUser = await User.findOne({ email: email });
         if (foundUser) {
             res.send("User exists");
-        }else{
+        } else {
             const newUser = new User({
-                name : name,
-                email : email,
-                userType : userType,
-                password : password
+                name: name,
+                email: email,
+                userType: userType,
+                password: password
             });
-           const savedUser = await newUser.save();
-           // these session datas are being saved in my mongoDB data base -- browser only storing session id in the Cokie - which will be sent to server in every request it makes
-           // when user is saved set session credentials
-           req.session.user_ID = savedUser._id; // adding user id to session
-           req.session.isAuth = true ; // setting authenticate property -true.. that will be cheked in the routes
-           req.session.userType = findUser.userType ; // setting user type as teacher or student - will be required to check in upload routing
-           // redirect according to user type page
-           if(savedUser.userType === "Student" ) {
-             res.redirect(`/student/${savedUser._id}`)
-           }else if(savedUser.userType === "Teacher"){
-             res.redirect(`/teacher/${savedUser._id}`)
-           }
+            const savedUser = await newUser.save();
+            // these session datas are being saved in my mongoDB data base -- browser only storing session id in the Cokie - which will be sent to server in every request it makes
+            // when user is saved set session credentials
+            req.session.user_ID = savedUser._id; // adding user id to session
+            req.session.isAuth = true; // setting authenticate property -true.. that will be cheked in the routes
+            req.session.userType = findUser.userType; // setting user type as teacher or student - will be required to check in upload routing
+            // redirect according to user type page
+            if (savedUser.userType === "Student") {
+                res.redirect(`/student/${savedUser._id}`)
+            } else if (savedUser.userType === "Teacher") {
+                res.redirect(`/teacher/${savedUser._id}`)
+            }
         }
-   } catch (error) {
-    console.log(error);
-   }
+    } catch (error) {
+        console.log(error);
+    }
 })
-app.get("/login",(req,res)=>{
-   // res.sendFile('public/login.html' , {root : __dirname});
-   res.render("login");
+app.get("/login", async (req, res) => {
+    // checks whether user is already logedIn or not
+    if (req.session.isAuth === true) {
+        const foundUser = await User.findOne({ _id: req.session.user_ID }).select('-password');
+        if (foundUser.userType === "Teacher") {
+            res.redirect(`/teacher/${foundUser._id}`)
+        } else if (foundUser.userType === "Student") {
+            res.redirect(`/student/${foundUser._id}`)
+        } else {
+            res.render("login");
+        }
+    } else {
+        res.render("login");
+    }
 })
-app.post("/login",async (req,res)=> {
+app.post("/login", async (req, res) => {
     // const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    const findUser = await User.findOne({email: email});
+    const findUser = await User.findOne({ email: email });
     if (findUser) {
         //check pasword
         if (findUser.password === password) {
@@ -129,74 +200,138 @@ app.post("/login",async (req,res)=> {
                 // sending teacher page
                 // when user is saved set session credentials
                 req.session.user_ID = findUser._id; // adding user id to session
-                req.session.isAuth = true ; // setting authenticate property -true.. that will be cheked in the routes
-                req.session.userType = findUser.userType ; // setting user type as teacher or student - will be required to check in upload routing
+                req.session.isAuth = true; // setting authenticate property -true.. that will be cheked in the routes
+                req.session.userType = findUser.userType; // setting user type as teacher or student - will be required to check in upload routing
                 res.redirect(`/teacher/${findUser._id}`)
-            }else{
+            } else {
                 // when user is saved set session credentials
                 req.session.user_ID = findUser._id; // adding user id to session
-                req.session.isAuth = true ; // setting authenticate property -true.. that will be cheked in the routes
-                req.session.userType = findUser.userType ; // setting user type as teacher or student - will be required to check in upload routing
+                req.session.isAuth = true; // setting authenticate property -true.. that will be cheked in the routes
+                req.session.userType = findUser.userType; // setting user type as teacher or student - will be required to check in upload routing
                 // sending student page 
                 res.redirect(`/student/${findUser._id}`)
             }
-        }else{
-            res.json({"msg" : "Entered wrong password."});
+        } else {
+            res.json({ "msg": "Entered wrong password." });
             res.redirect("/login");
         }
-    }else{
-        res.json({"msg" : "This user does not exists"});
+    } else {
+        res.json({ "msg": "This user does not exists" });
         res.redirect("/register");
     }
 });
-app.get('/logout',(req,res)=>{
+app.get('/logout', (req, res) => {
     // first check whether authorised or not
     if (req.session.isAuth === true) {
-        req.session.destroy((err)=>{
+        req.session.destroy((err) => {
             if (err) {
                 console.log(err);
-            }else{
+            } else {
                 console.log("User loged out successfully");
                 // redirect to home route
                 res.redirect('/');
             }
         })
-    }else{
-    // send log in page
-    res.redirect('/login');
+    } else {
+        // send log in page
+        res.redirect('/login');
     }
 })
 // get particular teacher & send teacher page
-app.get("/teacher/:id",async (req,res)=>{
+app.get("/teacher/:id", async (req, res) => {
     // first check whether user is authenticated or not
     if (req.session.isAuth === true) {
-        const foundUser = await User.findOne({_id:req.params.id});
-        res.render("teacher", {user:foundUser});
-    }else{
+        const foundUser = await User.findOne({ _id: req.params.id });
+        res.render("teacher", { user: foundUser });
+    } else {
         // user is not authenticated and send login page
         res.redirect("/login");
     }
 })
 // get particular student & send student page
-app.get("/student/:id",async (req,res)=>{
+app.get("/student/:id", async (req, res) => {
     if (req.session.isAuth === true) {
-        const foundUser = await User.findOne({_id:req.params.id});
-        res.render("student",{user:foundUser});
-    }else{
+        const foundUser = await User.findOne({ _id: req.params.id });
+        res.render("student", { user: foundUser });
+    } else {
         res.redirect('/login');
     }
 })
 
 
-app.get("/uploads/sem/:semNum", (req,res)=>{
+app.get("/uploads/sem/:semNum", (req, res) => {
     // here first check two things 1) whether user is authenti cated & 2) user is teacher or student --
     // bcoz if it is student it will be not allowed to upload any data
     const num = req.params.semNum;
     if (req.session.isAuth === true && req.session.userType === "Teacher") {
         // it is teacher & authenticated to upload docs
-        res.render("semupload",{semesterNum : num });
-    }else{
-         // it is not authenticated - send login page
+        res.render("semupload", { semesterNum: num });
+    } else {
+        // it is not authenticated - send login page
+        res.redirect("/login");
+    }
+});
+// to create random string
+// randomstring.generate({length:12,charset : 'alphanumeric'})
+// console.log(randomstring.generate({
+//     length: 12,
+//     charset: 'alphanumeric'
+//   }));
+app.post("/uploads/sem/:semNum", upload.single('sem_file'), async (req, res) => {
+    if (req.session.isAuth === true && req.session.userType === "Teacher") { //authorised
+        console.log(req.file);
+        // console.log(req.file.buffer);
+        // console.log(req.session.user_ID);
+        const foundUser = await User.findOne({ _id: req.session.user_ID }).select("-password"); // gives all things without password
+        // console.log(foundUser);
+        const relatedSem = req.params.semNum;
+        const userId = req.session.user_ID;
+        const uploadedBy = foundUser.name;
+        const docOriginalName = req.file.originalname;
+        let uploadedDocUrl = "";
+        const docType = req.file.mimetype;
+        const usesType = "study_material";
+        // Generate Random string -- to prevent overide of files
+        const now = new Date();
+        const dateStamp = now.toISOString();
+        const randomString = randomstring.generate({ length: 12, charset: "alphanumeric" })
+        const docGivenName = docOriginalName + dateStamp + randomString;
+        const metaData = {
+            contentType: req.file.mimetype
+        }
+        let uploadTask;
+        //-----------FILE UPLOADING STARTS ------------------
+        if (req.file.mimetype === "image/jpeg" || req.file.mimetype === "image/png") {
+            const imagesFolderRef = ref(imagesRef, `semester${relatedSem}/${docGivenName}`);
+            uploadTask = uploadBytes(imagesFolderRef, req.file.buffer, metaData);
+        } else if (req.file.mimetype === "application/pdf") {
+            const documentsFolderRef = ref(documentsRef, `semester${relatedSem}/${docGivenName}`);
+            uploadTask = uploadBytes(documentsFolderRef, req.file.buffer, metaData);
+        }
+        //-----------FILE UPLOADING ENDS ------------------
+        //-----------GET DOCUMENT URL----------------
+        await uploadTask.then((snapshot) => {
+            getDownloadURL(snapshot.ref).then(async (downloadURL) => {
+                // console.log('File available at', downloadURL);
+                uploadedDocUrl = downloadURL;
+                //----------UPLOAD ALL CREDENTIALS OF FILE TO MONGODB ---------
+                const dataOfUploadedMongo = new Document({
+                    userId: userId,
+                    uploadedBy: uploadedBy,
+                    relatedSem: relatedSem,
+                    docOriginalName: docOriginalName,
+                    docGivenName: docGivenName,
+                    uploadedDocUrl: uploadedDocUrl,
+                    docType: docType,
+                    usesType: usesType,
+                });
+                await dataOfUploadedMongo.save();
+                res.send("Data saved to mongoDB successfully");
+            });
+        });
+    }
+    // means unauthorised
+    else {
         res.redirect("/login");
     }
 })
@@ -204,6 +339,6 @@ app.get("/uploads/sem/:semNum", (req,res)=>{
 
 
 const port = 5000;
-app.listen(port,function(){
+app.listen(port, function () {
     console.log("Server started on port " + `${port}`);
 })
